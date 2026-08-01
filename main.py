2188dbf0e336d90907a924d7befb97e6804d5c99
Moeed Sarwar <moeedsarwar@192.168.1.5>
2026-08-01 07:39:54 +0500

    added greetings

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
 import typer
 from rich import print
-from core.client import client, ask_llm
+from core.client import ask_llm
 from config import constants
 
 app = typer.Typer()
 
 
 @app.command()
-def hello():
-    print(f"[bold cyan]Kage is now online.{client}[/bold cyan]")
+def kage():
+    greetings = ask_llm("greet me, tell me you name, and ask what i want")
+    print(f"Kage: {greetings}")
 
-
-@app.command()
-def bye():
-    print("[bold cyan]Kage is now shutting down..[/bold cyan]")
-
-
-@app.command()
-def ask():
     text = None
-    while text is None or text not in constants.quiting:
+    while text is None or text.lower() not in constants.quiting:
         text = input("You: ")
         if text.lower() in constants.quiting:
             break
